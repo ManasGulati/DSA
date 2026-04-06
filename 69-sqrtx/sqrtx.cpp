de8fc20dc3ee77b83e198281647193ba8d316162
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-04-06 17:13:46 +0530

    Time: 12 ms (11.73%) | Memory: 8.5 MB (49.69%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -3,7 +3,7 @@ public:
     int mySqrt(int x) {
         int possibility=0;
         for(long i=1;i<=x;i++){
-            if(long (i*i)<=x){
+            if((i*i)<=x){
                 possibility=i;
             }else{
                 break;
