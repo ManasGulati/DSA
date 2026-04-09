fca9f82d363e9fa31afd4b5970ead134a02141ff
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-04-09 20:46:19 +0530

    Time: 13 ms (39.36%) | Memory: 32 MB (84.53%) - LeetSync

--- 561-array-partition/array-partition.cpp
+++ 561-array-partition/array-partition.cpp
@@ -3,8 +3,8 @@ public:
     int arrayPairSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int sum=0;
-        for(auto i=nums.begin();i!=nums.end();i=i+2){
-            sum+=*(i);
+        for(auto it=nums.begin();it!=nums.end();it=it+2){
+            sum+=*(it);
         }
         return sum;
     }
