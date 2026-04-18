362c94d686f695e4fcdc2635d753057d7587026e
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-04-18 23:35:17 +0530

    Time: 1 ms (74.68%) | Memory: 67.7 MB (23.33%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -5,6 +5,7 @@ public:
         for(int i=0;i<nums.size();i++){
             arr[nums[i]]++; 
         }
+        
         for(int i=1;i<arr.size();i++){
             if(arr[i]>1){
                 return i;
