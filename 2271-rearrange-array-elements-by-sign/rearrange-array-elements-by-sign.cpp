1ca48d6abc1f6828220427f5a69ac4afb172f897
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-04-19 23:21:06 +0530

    Time: 0 ms (100.00%) | Memory: 128.2 MB (94.96%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -6,15 +6,14 @@ public:
         vector<int>ans(nums.size());
         for(int i=0;i<nums.size();i++){
             if(nums[i]>0){
-                if(pos<nums.size()){
-                    ans[pos]=nums[i];
-                }
+                
+                ans[pos]=nums[i];
                 pos+=2;
             }
             if(nums[i]<0){
-                if(neg<nums.size()){
-                    ans[neg]=nums[i];
-                }
+                
+                ans[neg]=nums[i];
+                
                 neg+=2;
             }
         }
