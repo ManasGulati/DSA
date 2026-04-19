1d35af4b46cfcd5a1853276fb64555ea4469f4e0
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-04-19 14:06:28 +0530

    Time: 0 ms (100.00%) | Memory: 30.6 MB (12.33%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector <int>a(nums.size());
+        for(int i=0;i<nums.size();i++){
+            a[(i+k)%nums.size()]=nums[i];
+        }
+        nums=a;
+    }
+};
