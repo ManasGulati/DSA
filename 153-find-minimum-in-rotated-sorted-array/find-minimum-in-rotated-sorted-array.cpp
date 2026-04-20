8a35d772b0409bf3dab1f64cf686260c33e68b7a
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-04-20 17:39:31 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (48.98%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -7,9 +7,7 @@ public:
         if(nums[0]<nums[nums.size()-1]){
             return nums[0];
         }
-        if(nums.size()==2&&nums[0]>nums[1]){
-            return nums[1];
-        }
+        
         int start=1,end=nums.size()-1;
         int mid=-1;
         while(start<=end){
