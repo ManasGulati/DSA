75b87263861e08bf5bfe4de8d23d5e1add43cf53
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-04-23 22:15:49 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (32.26%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.cpp
+++ 539-minimum-time-difference/minimum-time-difference.cpp
@@ -9,7 +9,6 @@ public:
         sort(min.begin(),min.end());
         int ans=INT_MAX;
         for(int i=0;i<min.size()-1;i++){
-
             if(min[i+1]-min[i]<ans){
                 ans=min[i+1]-min[i];
             }
