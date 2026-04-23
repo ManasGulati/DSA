1dda44a7dbfcda4532b00ad40c1cfa20c9676ae2
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-04-23 13:36:50 +0530

    Time: 38 ms (17.02%) | Memory: 12.5 MB (98.90%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        int i=0;
+        int n=s.length()-1;
+        while(i<n){
+            if(s[i]==s[i+1]){
+                s.erase(i,2);
+                i--;
+                if(i<0){
+                    i=0;
+                }
+                continue;
+            }
+            i++;
+        }
+        return s;
+    }
+};
