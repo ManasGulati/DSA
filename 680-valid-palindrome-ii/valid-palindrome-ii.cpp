f4e666d7180ad5f4bda218947eecec7e26aaacd7
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-04-23 17:11:38 +0530

    Time: 3 ms (69.79%) | Memory: 32 MB (5.50%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -9,13 +9,14 @@ public:
             }
             i++;
             j--;
+            
         }
         return true;
     }
 
     bool validPalindrome(string s) {
         int first=-1,last=-1;
-        
+
         int i=0,j=s.length()-1;
 
         while(i<j){
