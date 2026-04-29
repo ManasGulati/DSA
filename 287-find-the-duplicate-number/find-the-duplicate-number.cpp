5efa2d8f83eeae008ee5fa2ad5fe37e5145560ac
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-04-29 16:09:57 +0530

    Time: 7 ms (50.77%) | Memory: 65.1 MB (69.22%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -7,8 +7,9 @@ public:
             slow=nums[slow];
             fast=nums[nums[fast]];
         }while(slow!=fast);
-        
+
         slow=0;
+        
         while(slow!=fast){
             slow=nums[slow];
             fast=nums[fast];
