92dbc0775ce35442a04b860d007d84d213e898f5
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-04-29 16:04:26 +0530

    Time: 23 ms (11.74%) | Memory: 32.9 MB (5.51%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -5,10 +5,11 @@ public:
         int n=grid.size()*grid.size();
 
         int esum=(n*(n+1))/2;
-        
+
         int asum=0;
 
         unordered_set<int>s;
+        
         vector <int>ans;
 
         for(auto v:grid){
