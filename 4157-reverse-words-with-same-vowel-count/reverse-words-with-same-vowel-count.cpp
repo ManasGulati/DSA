93914ec4679347e66d1da1636ce471a87f0c433e
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-04-30 02:15:44 +0530

    Time: 19 ms (78.98%) | Memory: 35.3 MB (93.90%) - LeetSync

---
+++ 4157-reverse-words-with-same-vowel-count/reverse-words-with-same-vowel-count.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    void rev(string &s,int p,int q){
+        while(p<q){
+            swap(s[p],s[q]);
+            p++;
+            q--;
+        }
+    }
+    string reverseWords(string s) {
+        int vc=0;
+        int idx=-1;
+        if(s.find(' ')==string::npos){
+            return s;
+        }else{
+            idx=s.find(' ');
+        }
+        for(int i=0;i<idx;i++){
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
+                vc++;
+            }
+        }
+        for(int i=idx+1;i<s.length();i++){
+            int temp=0;
+            int si=i;
+            while(i<s.length()&&s[i]!=' '){
+                if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
+                    temp++;
+                }
+                i++;
+            }
+            if(temp==vc){
+                int p=si;
+                int q=i-1;
+                while(p<q){
+                    swap(s[p],s[q]);
+                    p++;
+                    q--;
+                }
+            }
+        }
+        return s;
+        
+    }
+};
