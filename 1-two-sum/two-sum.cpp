061712f216a5ba13ecfc342350ad85b69b05c5d3
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-04-29 15:23:30 +0530

    Time: 3 ms (66.56%) | Memory: 14.7 MB (62.89%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -3,6 +3,7 @@ public:
     vector<int> twoSum(vector<int>& nums, int t) {
         unordered_map <int,int> m;
         int idx=0;
+        
         for(auto val:nums){
             if(m.find(t-val)!=m.end()){
                 vector<int> ans={idx,m[t-val]};
