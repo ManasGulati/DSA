707571f23a7e16544e913255d7390daddda8dd28
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-04-29 17:42:57 +0530

    Time: 15 ms (91.30%) | Memory: 17.4 MB (82.41%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        
         vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size();i++){
