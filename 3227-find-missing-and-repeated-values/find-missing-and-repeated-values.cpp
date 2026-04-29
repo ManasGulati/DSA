b1b96050ecfad168829e13fc5fbc7c963a1d8a66
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-04-29 16:07:00 +0530

    Time: 15 ms (38.36%) | Memory: 32.8 MB (5.51%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -13,10 +13,12 @@ public:
         vector <int>ans;
 
         for(auto v:grid){
-            
+
             for(auto i:v){
+
                 asum+=i;
                 if(s.find(i)!=s.end()){
+                    
                     ans.push_back(i);
                 }
                 s.insert(i);
