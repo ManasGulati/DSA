fc1311e79cc91ebca89bf7b47d4eb062865fc946
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-04-30 23:19:45 +0530

    Time: 26 ms (24.81%) | Memory: 32.7 MB (14.18%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>m;
+        for(auto str:strs){
+            vector<int> freq(26,0);
+            string key;
+            for(auto ch:str){
+                freq[ch-'a']++;
+            }
+            for(int i=0;i<26;i++){
+                key.push_back(freq[i]+'\0');
+            }
+            if(m.find(key)!=m.end()){
+                m[key].push_back(str);
+            }else{
+                m[key]={str};
+            }
+        }
+        vector<vector<string>> ans;
+        for(auto it:m){
+            ans.push_back(it.second);
+        }
+        return ans;
+
+                
+    }
+};
