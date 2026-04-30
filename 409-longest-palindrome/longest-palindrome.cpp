da79a57d79fb6bd527de3c6aa997c20553912e70
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-05-01 01:39:17 +0530

    Time: 3 ms (17.52%) | Memory: 9.3 MB (39.04%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char,int>m;
+        for(char ch:s){
+            if(m.find(ch)==m.end()){
+                m[ch]=1;
+            }else{
+                m[ch]++;
+            }
+        }
+        int ans=0;
+        bool addone=false;
+        for(auto it:m){
+            if(it.second%2==0){
+                ans+=it.second;
+            }else if(it.second%2!=0){
+                ans+=(it.second/2)*2;
+                addone=true;
+            }
+        }
+        if(addone){
+            return ans+1;
+        }else{
+            return ans;
+        }
+        
+        
+    }
+};
