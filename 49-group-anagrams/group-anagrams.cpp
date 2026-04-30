8d40ac6343281fee5b0a723a4205f4fe44edaa20
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-04-30 23:22:25 +0530

    Time: 23 ms (30.71%) | Memory: 32.7 MB (14.07%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        
+
         unordered_map<string,vector<string>>m;
 
         for(auto str:strs){
@@ -23,6 +23,7 @@ public:
         vector<vector<string>> ans;
 
         for(auto it:m){
+            
             ans.push_back(it.second);
         }
 
