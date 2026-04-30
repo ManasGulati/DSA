64c0a922e85f08881ca24cdeef3958779cc8da46
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-04-30 19:07:55 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (44.91%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int start=0,end=nums.size()-1;
-        while(start<=end){
-            int mid=start + ((end-start)/2);
-            if(nums[mid]==target){
-                return mid;
-            }else if(nums[mid]>target){
-                end=mid-1;
-            }else{
-                start=mid+1;
-            }
+    int bs(vector<int>& nums,int start,int end,int t){
+        //base case
+        if(start>end){
+            return -1;
         }
-        return -1;
+
+        //processing
+        int mid=start+(end-start)/2;
+
+        //recursive call
+        if(nums[mid]==t){
+            return mid;
+        }else if(nums[mid]>t){
+            return bs(nums,start,mid-1,t);
+        }else{
+            return bs(nums,mid+1,end,t);
+        }
+        
+
+    }
+    int search(vector<int>& nums, int target) {
+        int ans=bs(nums,0,nums.size()-1,target);
+        return ans;
+
     }
 };
