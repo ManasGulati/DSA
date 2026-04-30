1a0436469ea70ea0b3afdf7d63d97dcd92634e50
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-04-30 19:31:20 +0530

    Time: 1 ms (53.27%) | Memory: 9.3 MB (54.88%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         unordered_map <char,char> m;
-        
+
         unordered_set <char> set;
+
         for(int i=0;i<s.size();i++){
+            
 
             if(m.find(s[i])==m.end()&&set.find(t[i])!=set.end()){
                 return false;
