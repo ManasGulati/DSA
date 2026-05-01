a1fc0b77e68668dc9fc3da71278c4e16486f087f
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-05-01 15:48:58 +0530

    Time: 4 ms (13.45%) | Memory: 16.4 MB (9.58%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void solve(vector<int>v,int idx,vector<int>& ans,vector<vector<int>>& final){
+        if(idx>=v.size()){
+            final.push_back(ans);
+            return;
+        }
+        ans.push_back(v[idx]);
+        solve(v,idx+1,ans,final);
+        ans.pop_back();
+        while(idx<v.size()-1&&v[idx]==v[idx+1]){
+            idx++;
+        }
+        solve(v,idx+1,ans,final);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int> ans;
+        vector<vector<int>> final;
+        solve(nums,0,ans,final);
+        return final;
+        
+    }
+};
