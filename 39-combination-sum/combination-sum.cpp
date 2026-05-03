5907f5fa1feafb75a33e94d98e5d2fb649ea9eaa
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-05-03 21:05:04 +0530

    Time: 43 ms (12.27%) | Memory: 46.3 MB (9.02%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    void solve(vector<int> arr, int t, vector<int> &ans, vector<vector<int>> &fin,int i){
+        //base case
+        if(t==0){
+            fin.push_back(ans);
+            return;
+        }
+        if(t<0||i>=arr.size()){
+            return;
+        }
+        
+
+        //recursion call
+        
+        //include
+        if(arr[i]<=t){
+            ans.push_back(arr[i]);
+            solve(arr,t-arr[i],ans,fin,i);
+            ans.pop_back();
+        }
+
+        //exclude
+        solve(arr,t,ans,fin,i+1);
+        
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& arr, int t) {
+        vector<int> ans;
+        vector<vector<int>> fin;
+
+        solve(arr,t,ans,fin,0);
+        return fin;
+        
+    }
+};
