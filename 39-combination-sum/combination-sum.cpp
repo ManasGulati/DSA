9ae2da9bbb80f8d52b465df6eba53716d1292db6
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-05-03 21:05:35 +0530

    Time: 44 ms (11.78%) | Memory: 46.2 MB (9.02%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 
     void solve(vector<int> arr, int t, vector<int> &ans, vector<vector<int>> &fin,int i){
-        
+
         //base case
         if(t==0){
             fin.push_back(ans);
@@ -19,6 +19,7 @@ public:
         if(arr[i]<=t){
             ans.push_back(arr[i]);
             solve(arr,t-arr[i],ans,fin,i);
+            
             ans.pop_back();
         }
 
