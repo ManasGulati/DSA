89711ec5917d1ed8dd8d05b9ea23eaf7d0b2e69c
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-05-03 06:05:16 +0530

    Time: 8 ms (46.49%) | Memory: 33 MB (16.70%) - LeetSync

---
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        int ans=0;
+        int mul1=-1;
+        int mul2=-1;
+        bool er=true;
+        for(auto str:bank){
+            int count=0;
+            er=true;
+            for(auto ch:str){
+                if(ch=='1'){
+                    count++;
+                    er=false;
+                }
+            }
+            if(er==false){
+                if(mul1==-1){
+                    mul1=count;
+                }else if(mul2==-1){
+                    mul2=count;
+                    ans+=(mul1*mul2);
+                    mul1=mul2;
+                    mul2=-1;
+                }
+            }
+        }
+        return ans;
+
+        
+    }
+};
