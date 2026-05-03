44de25c310672c7d85249f939a3ae1921c11a0bf
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-05-03 23:56:48 +0530

    Time: 3 ms (21.21%) | Memory: 11.6 MB (6.15%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -17,12 +17,14 @@ public:
         int ans=0;
 
         int idx=1;
-        
+
         for(auto ch:s){
            
             ans+=(m[ch]*idx);
+            
             idx++;
         }
+
         return ans;
         
     }
