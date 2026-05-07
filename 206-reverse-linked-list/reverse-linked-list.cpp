c5236011eb5f8333f89e18ed7087245e1b2d1fe4
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-05-07 14:29:46 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (40.54%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -10,6 +10,23 @@
  */
 class Solution {
 public:
+    void solve(ListNode *&next, ListNode *&temp, ListNode * &pre,ListNode* &head){
+        //base case
+        if(temp==NULL){
+            head=next;
+            return;
+        }
+
+        //recursion call
+        temp->next=next;
+        ListNode *pre2;
+        if(pre!=NULL){
+            pre2=pre->next;
+        }else{
+            pre2=NULL;
+        }
+        solve(temp,pre,pre2,head);
+    }
     ListNode* reverseList(ListNode* head) {
         ListNode *next=NULL;
         ListNode *temp=head;
@@ -19,18 +36,8 @@ public:
         }else{
             pre=NULL;
         }
+        solve(next,temp,pre,head);
 
-        while(temp!=NULL){
-            temp->next=next;
-            next=temp;
-            temp=pre;
-            if(pre!=NULL){
-                pre=pre->next;
-
-            }
-            
-        }
-        head=next;
         return head;
 
         
