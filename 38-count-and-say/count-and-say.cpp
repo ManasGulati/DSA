56b4a8366bd5202b7427ab14c70d33b9da3e80d0
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-05-08 15:31:01 +0530

    Time: 4 ms (51.24%) | Memory: 10.1 MB (26.72%) - LeetSync

---
+++ 38-count-and-say/count-and-say.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    string solve(string &s,int n){
+        //base case
+        if(n==1){
+            return s;
+        }
+
+        //recursion
+        string a=solve(s,n-1);
+        char element=a[0];
+        string b;
+        int count=0;
+        
+        for(int i=0;i<a.length();i++){
+            
+            if(a[i]==element){
+                count++;
+            }else{
+                b+=to_string(count);
+                b+=element;
+                element=a[i];
+                count=1;
+            }
+        }
+        b+=to_string(count);
+        b+=element;
+        return b;
+
+    }
+    
+    string countAndSay(int n) {
+        string s="1";
+        return solve(s,n);
+
+
+        
+    }
+};
