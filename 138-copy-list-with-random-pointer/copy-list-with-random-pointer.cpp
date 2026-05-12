9b185a3b24b4785cb6633d6f89f231fc86122f09
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-05-12 21:41:36 +0530

    Time: 7 ms (51.68%) | Memory: 15.2 MB (46.44%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -0,0 +1,66 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(head==NULL){
+            return NULL;
+        }
+        Node *head2=NULL;
+        Node*temp=head;
+        
+        vector<Node*>map;
+        vector<Node*>org;
+        while(temp!=NULL){
+            org.push_back(temp);
+            temp=temp->next;
+        }
+        temp=head;
+        while(temp!=NULL){
+            Node*n=new Node(temp->val);
+            map.push_back(n);
+            temp=temp->next;
+        }
+        for(int i=0;i<map.size()-1;i++){
+            if(head2==NULL){
+                head2=map[i];
+            }
+            map[i]->next=map[i+1];
+            if(org[i]->random!=NULL){
+                auto it=find(org.begin(),org.end(),org[i]->random);
+                map[i]->random=map[distance(org.begin(),it)];
+            }else{
+                map[i]->random=NULL;
+            }
+        }
+        if(org[org.size()-1]->random!=NULL){
+            if(head2==NULL){
+                head2=map[org.size()-1];
+            }
+            auto it=find(org.begin(),org.end(),org[org.size()-1]->random);
+            map[org.size()-1]->random=map[distance(org.begin(),it)];
+        }else{
+            if(head2==NULL){
+                head2=map[org.size()-1];
+            }
+
+        }
+        
+        return head2;
+        
+    }
+};
