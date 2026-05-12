3020474603ce9cfbbf334dfd5faa1c2e22cefc97
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-05-12 18:34:02 +0530

    Time: 80 ms (12.64%) | Memory: 28.6 MB (14.22%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -15,11 +15,12 @@ public:
 
         ListNode*ans=NULL;
         unordered_set<ListNode*> s;
-        
+
         while(t1!=NULL){
             s.insert(t1);
             t1=t1->next;
         }
+        
         while(t2!=NULL){
             if(s.count(t2)){
                 ans=t2;
