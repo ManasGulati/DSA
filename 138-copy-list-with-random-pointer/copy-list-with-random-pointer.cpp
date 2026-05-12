76377ef62c160daf577437515c85be587cc2ade1
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-05-12 21:43:49 +0530

    Time: 11 ms (14.91%) | Memory: 15.2 MB (26.69%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -45,13 +45,14 @@ public:
                 map[i]->random=map[distance(org.begin(),it)];
             }
         }
+        
         if(org[org.size()-1]->random!=NULL){
             auto it=find(org.begin(),org.end(),org[org.size()-1]->random);
             map[org.size()-1]->random=map[distance(org.begin(),it)];
         }
         if(head2==NULL){
-                head2=map[org.size()-1];
-            }
+            head2=map[org.size()-1];
+        }
         
         return head2;
         
