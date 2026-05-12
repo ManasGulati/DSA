ad3cc4b6f46e3d60192f572a099058b539707b62
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-05-12 18:49:37 +0530

    Time: 8 ms (83.49%) | Memory: 11.9 MB (51.93%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -11,7 +11,7 @@ public:
     bool hasCycle(ListNode *head) {
 
         //trying to get 0 ms by repeated submission.Adding comment to make code differ
-        //cc
+        //cc41651111
 
         if(head==NULL){
             return false;
