2f014935fe63446ab19ea4ed941c7856fd0b8449
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-05-15 17:22:42 +0530

    Time: 4 ms (78.18%) | Memory: 161.5 MB (62.23%) - LeetSync

---
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    void solve(ListNode*&hd){
+        ListNode*head=hd;
+        
+        while(head->next!=NULL){
+            ListNode* next=head->next;
+            if(next!=NULL&&head->val>next->val){
+                head->next=next->next;
+                next->next=NULL;
+                continue;
+            }
+            if(head->next!=NULL)head=head->next;
+        }
+    }
+    void reverse(ListNode*&head){
+        ListNode*prev=NULL;
+        ListNode*next=head->next;
+        while(head->next!=NULL){
+            head->next=prev;
+            prev=head; 
+            head=next;
+            next=next->next;               
+            
+        }
+        head->next=prev;
+    }
+
+    ListNode* removeNodes(ListNode* head) {
+        reverse(head);
+
+        solve(head);
+
+
+        reverse(head);
+
+        return head;
+        
+    }
+};
