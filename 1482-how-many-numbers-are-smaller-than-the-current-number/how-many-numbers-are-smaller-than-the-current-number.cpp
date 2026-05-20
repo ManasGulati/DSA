ddd7582b44ef779e8e8acf70359de6d066e629e3
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-05-20 14:32:01 +0530

    Time: 2 ms (70.58%) | Memory: 14.5 MB (23.11%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -4,14 +4,17 @@ public:
         vector<int>dup(nums);
         sort(dup.begin(),dup.end());
         unordered_map<int,int>m;
+
         for(int i=0;i<dup.size();i++){
             if(m.find(dup[i])==m.end()){
                 m[dup[i]]=i;
             }
         }
+
         for(int i=0;i<nums.size();i++){
             dup[i]=m[nums[i]];            
         }
+        
         return dup;
         
         
