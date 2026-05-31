71e80c104d52a783c417668484518e600e2f50d8
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-05-31 13:19:13 +0530

    Time: 4 ms (58.15%) | Memory: 30.4 MB (5.48%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& n) {
+        vector<int>ans(n);
+        stack<int>s;
+        ans.insert(ans.end(),n.begin(),n.end());
+        int idx=n.size()-1;
+        int i=ans.size()-1;
+
+        while(i>=0){
+            int ge=-1;
+            while(!s.empty()){
+                if(s.top()>ans[i]){
+                    ge=s.top();
+                    break;
+                }else{
+                    s.pop();
+                }                  
+            }
+            s.push(ans[i]);
+            if(idx<0){
+                idx=n.size()-1;
+            }
+            n[idx]=ge;
+            s.push(ans[i]);
+            i--;
+            idx--;
+        }
+
+
+        
+        return n;
+        
+    }
+};
