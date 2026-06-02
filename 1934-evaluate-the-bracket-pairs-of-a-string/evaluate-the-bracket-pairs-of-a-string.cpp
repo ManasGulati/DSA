da3de923ef6a4b0a3cec8f538cf4b75d0cf45975
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-06-02 16:08:16 +0530

    Time: 76 ms (80.42%) | Memory: 122.7 MB (67.08%) - LeetSync

---
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string evaluate(string s, vector<vector<string>>& k) {
+        unordered_map<string,string>m;
+        string key;
+        string ans;
+        bool flag=false;
+        for(int i=0;i<k.size();i++){
+            m[k[i][0]]=k[i][1];
+        }
+        for(char ch:s){
+            if(ch=='('){
+                flag=true;
+                continue;
+            }else if(ch==')'){
+                flag=false;
+                if(m.find(key)==m.end()){
+                    ans.push_back('?');
+                }else{
+                    ans.append(m[key]);
+                }
+                key.clear();                
+                continue;
+            }
+            if(!flag){
+                ans.push_back(ch);
+                
+            }else{
+                key.push_back(ch);
+            }
+        }
+        return ans;
+        
+    }
+};
