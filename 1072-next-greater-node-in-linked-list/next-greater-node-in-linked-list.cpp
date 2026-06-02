d2a694ac455aaea6c35f18538c83ab7dac251cb8
Manas Gulati <145512921+ManasGulati@users.noreply.github.com>
2026-06-02 18:32:40 +0530

    Time: 7 ms (54.98%) | Memory: 46.9 MB (46.11%) - LeetSync

--- 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.cpp
+++ 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.cpp
@@ -12,16 +12,18 @@ class Solution {
 public:
     vector<int> nextLargerNodes(ListNode* head) {
         vector<int>l;
+        
         while(head!=NULL){
             l.push_back(head->val);
             head=head->next;
         }
+
         stack<int>s;
         vector<int>ans(l.size());
         int count=l.size()-2;
         s.push(l[l.size()-1]);
         ans[l.size()-1]=0;
-        
+
         while(count>=0){
 
             if(!s.empty()&&l[count]<s.top()){
